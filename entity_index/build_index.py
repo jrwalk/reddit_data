86915287f89259f4f9b2ff17a0745a91291f8488
John Walk <johnrwalk@gmail.com>
2016-08-30 13:31:56 -0400

    keyword update, parallelization

---
+++ entity_index/build_index.py
@@ -0,0 +1,128 @@
+"""builds keyword indexer for reddit data.
+"""
+
+
+import pymysql as pms
+import nltk
+import language_tools as lt
+import json
+
+
+def get_texts(limit=None):
+    """getter method for pulling comment and submission texts.
+
+    KWARGS:
+        limit: int or None.
+            optional cap on number of posts to pull.
+
+    YIELDS:
+        texts: tuple.
+            tuple of (id,body) for comments and submissions
+    """
+    conn = pms.connect(host='localhost',
+                       user='root',
+                       passwd='',
+                       db='reddit',
+                       charset='utf8mb4',
+                       init_command='SET NAMES UTF8MB4')
+    cur = conn.cursor(pms.cursors.DictCursor)
+
+    if limit is not None:
+        limtext = " LIMIT {}".format(limit)
+    else:
+        limtext = ""
+
+    # exclude indexing comments already in the index
+    #query = """SELECT c.id, c.body FROM Comments c 
+    #            WHERE NOT EXISTS (
+    #                SELECT NULL FROM Mentions m 
+    #                    WHERE m.id=c.id;
+    #                ){};
+    #        """.format(limtext)
+    query = "SELECT c.id,c.body FROM Comments c{};".format(limtext)
+    cur.execute(query)
+    for row in cur:
+        yield (row['id'],row['body'])
+
+    #query = """SELECT s.id, s.title, s.selftext FROM Submissions s 
+    #            WHERE NOT EXISTS (
+    #                SELECT NULL FROM Mentions m 
+    #                    WHERE m.id=s.id;
+    #                ){};
+    #        """.format(limtext)
+    query = "SELECT s.id,s.title,s.selftext FROM Submissions s{}".format(limtext)
+    cur.execute(query)
+    for row in cur:
+        yield (row['id'],"{} {}".format(row['title'],row['selftext']))
+
+
+def build_keywords():
+    """builds master list of keywords to check.
+
+    RETURNS:
+        keywords: list.
+            List of strings of individual keyword terms.
+    """
+    with open('banks.json','r') as f:
+        banks = json.load(f)['banks']
+
+    with open('cards.json','r') as f:
+        cards = json.load(f)['issuer']
+
+    keywords = []
+
+    for bank in banks:
+        keywords.append(bank['name'])
+        alts = bank.get('alts',None)
+        if alts is not None:
+            keywords += alts
+
+    for card in cards:
+        for instance in card['cards']:
+            keywords.append(instance['name'])
+            alts = instance.get('alts',None)
+            if alts is not None:
+                keywords += alts
+
+    for mwe in lt.multi_word_entities():
+        term = ' '.join(mwe)
+        keywords.append(term)
+
+    keywords = set(keywords)
+
+    # hack -- remove common single-word entities to avoid false positives
+    removals = ['CARD','DC','FREEDOM','GOLD','GREEN','INFINITE','IT','OPEN',
+                'SECURED','JOURNEY','SPARK']
+    keywords.difference_update(set(removals))
+
+    return keywords
+
+
+def find_card_keywords(text,keys,stopwords=None):
+    """Checks text body for keywords associated with banks/cards, generates map 
+    of mentioned keywords.  Lightweight output to allow for parallelization 
+    before SQL write.
+
+    ARGS:
+        text: tuple.
+            tuple of (id,body) output yielded by `get_texts`.
+        keys: set.
+            set of keywords constructed by `build_keywords`.
+
+    RETURNS:
+        map: tuple or None.
+            tuple of (id,[keywords]).  If no keyword matches are found, 
+            returns None.
+    """
+    index,body = text
+    body = lt.tokenize(body,stopwords=stopwords)
+
+    keywords = []
+    for token in body:
+        if token in keys:
+            keywords.append(token)
+
+    if len(keywords) > 0:
+        return (index,keywords)
+    else:
+        return None
---
+++ entity_index/language_tools.py
@@ -0,0 +1,125 @@
+"""Processor for bank/card issuer and credit card mentions, building index 
+"""
+
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
+import json
+
+
+_lemmatizer = WordNetLemmatizer()
+
+def stop_words():
+    """wrapper to return expanded set of stopwords.
+
+    RETURNS:
+        stops: set.
+            set of stopword strings.
+    """
+    stops = stopwords.words('english')
+    expansion = [
+        'com',
+        'http',
+        'https',
+        'www'
+        'amp',
+        'gt',
+        'lt',
+        'le'
+    ]
+    for s in string.punctuation:
+        expansion.append(s)
+
+    stops = stops+expansion
+    stops = [s.upper() for s in stops]
+    return set(stops)
+
+
+def multi_word_entities():
+    """generates set of multi-word entities to be passed to the tokenizer
+
+    RETURNS:
+        MWE: list.
+            List of multi-word entities, with each entity expressed as a tuple 
+            of tokens.
+    """
+    with open('banks.json','r') as f:
+        banks = json.load(f)
+
+    with open('cards.json','r') as f:
+        cards = json.load(f)
+
+    # include keywords.json here
+
+    MWE = []
+
+    # process bank names
+    bank_lookup = {}
+    for bank in banks['banks']:
+        altnames = bank.get('alts',None)
+        bank_lookup[bank['name']] = altnames
+        if altnames is not None:
+            for name in altnames:
+                name = name.split()
+                if len(name) > 1:
+                    MWE.append(tuple(name))
+
+    # process card names
+    for issuer in cards['issuer']:
+        issuer_name = issuer['name']
+        issuer_alt_names = bank_lookup[issuer_name]
+        for card in issuer['cards']:
+            cardname = card['name']
+            altnames = card.get('alts',None)
+
+            MWE.append((issuer_name,cardname))
+            if issuer_alt_names is not None:
+                for ian in issuer_alt_names:
+                    newname = ian.split() + [cardname]
+                    MWE.append(tuple(newname))
+
+            if altnames is not None:
+                for name in altnames:
+                    name = name.split()
+                    if len(name) > 1:   # MWE in its own right
+                        MWE.append(tuple(name))
+                    else:               # MWE with the bank name
+                        MWE.append((issuer_name,name[0]))
+                        if issuer_alt_names is not None:
+                            for ian in issuer_alt_names:
+                                newname = ian.split() + name
+                                MWE.append(tuple(newname))
+
+    return MWE
+
+
+def tokenize(text,lemma=False,stopwords=None):
+    """tokenizes the input text.
+
+    ARGS:
+        text: string.
+            raw text to be tokenized.
+
+    KWARGS:
+        lemma: boolean.
+            Flag to lemmatize the terms.  Default False.
+        stopwords: boolean.
+            Flag to remove stopwords from tokens.  Default True.
+
+    RETURNS:
+        tokens: list.
+            List of upper-case tokens, represented as strings.
+    """
+    tokens = nltk.RegexpTokenizer(r'\w+').tokenize(text.upper())
+    tokens = nltk.MWETokenizer(mwes=multi_word_entities(),separator=' ').tokenize(tokens)
+
+    if stopwords is not None:
+        tokens = [word for word in tokens if word not in stopwords]
+
+    if lemma:
+        tokens = [_lemmatizer.lemmatize(word,pos='v') for word in tokens]
+        tokens = [_lemmatizer.lemmatize(word,pos='n') for word in tokens]
+
+    return tokens
